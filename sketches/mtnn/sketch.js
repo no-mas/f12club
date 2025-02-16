b6a15da6ed985b6f68cb10e7e08f8b141fbd074e
Rob Bryant <rob.bryant@gmail.com>
2025-02-16 14:52:49 -0800

    Adding another sketch (mtnn) and simple navigation.

---
+++ sketches/mtnn/sketch.js
@@ -0,0 +1,25 @@
+let xoff = 0.0;
+
+function setup() {
+  createCanvas(900, 600);
+  background(228, 224, 211);
+  stroke(17, 126, 157);
+  strokeWeight(1.3);
+  noFill();
+  frameRate(6);
+}
+
+function draw() {
+  background(228, 224, 211);
+  for (let x = 0; x < width; x+= 5) {
+    let xh = noise(xoff) * height;
+    line(x, 0, x, xh);
+    xoff += 0.01;
+  }
+  //stroke(6, 174, 213);
+  for (let x = 3; x < width; x += 5) {
+    let xh = noise(xoff) * height;
+    line(x, height, x, height-xh);
+    xoff += 0.01;
+  }
+}
